16d2c4d46aa91d83945e8a27069cfb3d9c8cfdb1
Erik Miranda <1644114+hakuya@users.noreply.github.com>
2011-03-08 05:44:55 -0500

    Adding higu into revision management

---
+++ lib/db.py
@@ -0,0 +1,433 @@
+import sqlite3
+import re
+
+def check_name( str ):
+
+    assert re.search( '[ ,()]', str[0] ) == None
+
+def check_type( str ):
+
+    assert re.search( '[,()]', str[1] ) == None
+
+class QueryError:
+
+    def __init__( self, ex ):
+
+        self.ex = ex
+
+class Query:
+
+    def __init__( self, select, table ):
+
+        self.select = select
+        self.table  = table
+        self.db     = table.db
+
+    def build_query( self ):
+
+        return self.select.build_query( self.table )
+
+    def eval( self, single_col = False, single_row = False ):
+
+        if( single_row ):
+            try:
+                if( single_col ):
+                    return self.__iter__().next()[0]
+                else:
+                    return self.__iter__().next()
+            except StopIteration:
+                return None
+        else:
+            if( single_col ):
+                return [r[0] for r in self.__iter__()]
+            else:
+                return [r for r in self.__iter__()]
+
+    def __iter__( self ):
+
+        q, k = self.build_query()
+
+        try:
+            c = self.db.cursor()
+            if( k == None ):
+                c.execute( q )
+            else:
+                c.execute( q, k )
+
+            return ResultSet( c )
+
+        except sqlite3.OperationalError, ex:
+            raise QueryError( ex )
+
+class ResultSet:
+
+    def __init__( self, rs ):
+
+        self.rs = rs
+
+    def __iter__( self ):
+
+        return self
+
+    def next( self ):
+
+        return self.rs.next()
+
+class Operator:
+
+    def make_query( self ):
+
+        return '', []
+
+class ComparisonOperator( Operator ):
+
+    def __init__( self, operator, lhs, rhs ):
+
+        self.operator = operator
+        self.lhs = lhs
+        self.rhs = rhs
+
+    def make_query( self ):
+
+        return '%s %s ?' % ( self.lhs, self.operator, ), [ self.rhs ]
+
+class InequalityOperator( ComparisonOperator ):
+
+    def __init__( self, lhs, rhs ):
+
+        ComparisonOperator.__init__( self, '!=', lhs, rhs )
+
+class LogicOperator( Operator ):
+
+    def __init__( self, operator, constraints ):
+
+        self.operator = operator
+        self.constraints = constraints
+
+    def make_query( self ):
+
+        s = ''
+        p = []
+
+        for c in self.constraints:
+
+            if( isinstance( c, Operator ) ):
+                q = c.make_query()
+
+                s += '(' + q[0] + ') %s ' % ( self.operator )
+                p.extend( q[1] )
+            else:
+                check_name( c[0] )
+
+                s += '%s = ? %s ' % ( c[0], self.operator )
+                p.append( c[1] )
+
+        s = s[:-(len( self.operator ) + 2)]
+
+        return s, p
+
+class AndOperator( LogicOperator ):
+
+    def __init__( self, constraints ):
+
+        LogicOperator.__init__( self, 'AND', constraints )
+
+class OrOperator( LogicOperator ):
+
+    def __init__( self, constraints ):
+
+        LogicOperator.__init__( self, 'OR', constraints )
+
+class InOperator( Operator ):
+
+    def __init__( self, col, query, neg = False ):
+
+        self.col = col
+        self.query = query
+        self.neg = neg
+
+    def make_query( self ):
+
+        q, k = self.query.build_query()
+
+        if( self.neg ):
+            return '%s not in (%s)' % ( self.col, q ), k
+        else:
+            return '%s in (%s)' % ( self.col, q ), k
+
+class LeftOuterJoinOperator( Operator ):
+
+    def __init__( self, table_a, table_b, name_a, name_b, var_a, var_b = None ):
+
+        self.db = table_a.db
+        self.table_a = table_a
+        self.table_b = table_b
+        self.name_a = name_a
+        self.name_b = name_b
+        self.var_a = var_a
+        if( var_b == None ):
+            self.var_b = var_a
+        else:
+            self.var_b = var_b
+
+    def make_query( self ):
+
+        q = ''
+        k = []
+
+        if( isinstance( self.table_a, Query ) ):
+            qt, kt = self.table_a.build_query()
+            q += '(%s) %s' % ( qt, self.name_a )
+            k.extend( kt )
+        else:
+            q += '%s %s' % ( self.table_a.name, self.name_a )
+
+        q += ' LEFT OUTER JOIN '
+
+        if( isinstance( self.table_b, Query ) ):
+            qt, kt = self.table_b.build_query()
+            q += '(%s) %s' % ( qt, self.name_b )
+            k.extend( kt )
+        else:
+            q += '%s %s' % ( self.table_b.name, self.name_b )
+
+        q += ' ON %s.%s = %s.%s' % ( self.name_a, self.var_a, self.name_b, self.var_b, )
+
+        return q, k
+
+class Selection:
+
+    def __init__( self, rows = None, query = None, order = None, group = None, descending = False, distinct = False ):
+
+        self.rows = rows
+        self.query = query
+        self.order = order
+        self.group = group
+        self.descending = descending
+        self.distinct = distinct
+
+    def build_query( self, table ):
+
+        if( self.rows == None ):
+            sel = '*'
+        else:
+            sel = ''
+
+            for row in self.rows:
+                check_name( row )
+                sel += row + ', '
+
+            sel = sel[:-2]
+
+        q = 'SELECT '
+        k = []
+
+        if( self.distinct ):
+            q += 'DISTINCT '
+
+        if( isinstance( table, Table ) ):
+            q += '%s FROM %s' % ( sel, table.name )
+        elif( isinstance( table, Operator ) ):
+            qt, kt = table.make_query()
+            q += '%s FROM %s' % ( sel, qt )
+            k.extend( kt )
+
+        if( self.query == None or len( self.query ) == 0 ):
+            pass
+        else:
+            if( not isinstance( self.query, Operator ) ):
+                query = AndOperator( self.query )
+            else:
+                query = self.query
+
+            query, constraints = query.make_query()
+
+            q += ' WHERE ' + query
+            k.extend( constraints )
+
+        if( self.group != None ):
+            check_name( self.group )
+
+            q += ' GROUP BY %s' % ( self.group )
+
+        if( self.order != None ):
+            check_name( self.order )
+
+            q += ' ORDER BY %s' % ( self.order )
+
+            if( self.descending != None ):
+                q += ' DESC'
+
+        return q, k
+
+class SqlLiteDatabase:
+
+    def __init__( self, dbfile ):
+
+        self.dbfile = dbfile
+        self.db = sqlite3.connect( self.dbfile )
+
+    def close( self ):
+
+        self.db.close()
+
+    def commit( self ):
+
+        self.db.commit()
+
+    def get_table( self, name ):
+
+        return Table( self.db, name )
+
+class Table:
+
+    def __init__( self, db, name ):
+
+        check_name( name )
+
+        self.db = db
+        self.name = name
+
+    def create( self, cols, temp = False ):
+
+        s = ''
+
+        for col in cols:
+            check_name( col[0] )
+            check_type( col[1] )
+
+            s += col[0] + ' ' + col[1] + ', '
+
+        s = s[:-2]
+
+        try:
+            c = self.db.cursor()
+            if( temp ): 
+                c.execute( 'CREATE TEMPORARY TABLE %s (%s)' % ( self.name, s ) )
+            else:
+                c.execute( 'CREATE TABLE %s (%s)' % ( self.name, s ) )
+        except sqlite3.OperationalError, ex:
+            raise QueryError( ex )
+
+    def drop( self ):
+
+        try:
+            c = self.db.cursor()
+            c.execute( 'DROP TABLE %s' % ( self.name ) )
+        except sqlite3.OperationalError, ex:
+            raise QueryError( ex )
+
+    def add_col( self, name, type ):
+
+        check_name( name )
+        check_type( type )
+
+        try:
+            c = self.db.cursor()
+            c.execute( 'ALTER TABLE %s ADD %s %s' % ( self.name, name, type ) )
+        except sqlite3.OperationalError, ex:
+            raise QueryError( ex )
+
+    def insert( self, rows ):
+
+        s = ''
+        t = ''
+
+        for row in rows:
+            check_name( row[0] )
+
+            s += row[0] + ','
+            t += '?,'
+
+        s = s[:-1]
+        t = t[:-1]
+
+        try:
+            c = self.db.cursor()
+            c.execute( 'INSERT INTO %s(%s) VALUES (%s)' % ( self.name, s, t ), map( lambda x: x[1], rows ) )
+        except sqlite3.OperationalError, ex:
+            raise QueryError( ex )
+
+    def insert_selection( self, selection, table, rows = None ):
+
+        if( rows != None and len( rows ) > 0 ):
+            s = ''
+
+            for row in rows:
+                check_name( row )
+
+                s += row + ','
+
+            s = s[:-1]
+        else:
+            s = None
+
+        q, k = selection.build_query( table.name )
+
+        if( s != None ):
+            q = 'INSERT INTO %s(%s) %s' % ( self.name, s, q )
+        else:
+            q = 'INSERT INTO %s %s' % ( self.name, q )
+
+        try:
+            c = self.db.cursor()
+            if( k != None ):
+                c.execute( q, k )
+            else:
+                c.execute( q )
+        except sqlite3.OperationalError, ex:
+            raise QueryError( ex )
+
+    def delete( self, query ):
+
+        q = 'DELETE FROM %s WHERE ' % ( self.name )
+
+        if( not isinstance( query, Operator ) ):
+            query = AndOperator( query )
+
+        query, constraints = query.make_query()
+
+        q += query
+
+        try:
+            c = self.db.cursor()
+            c.execute( q, constraints )
+        except sqlite3.OperationalError, ex:
+            raise QueryError( ex )
+
+    def update( self, rows, query = None ):
+
+        q = 'UPDATE %s SET ' % ( self.name )
+        p = []
+
+        for row in rows:
+            check_name( row[0] )
+
+            q += '%s = ?, ' % ( row[0] )
+            p.append( row[1] )
+
+        q = q[:-2]
+
+        if( query != None ):
+            q = q + ' WHERE '
+
+            if( not isinstance( query, Operator ) ):
+                query = AndOperator( query )
+
+            query, constraints = query.make_query()
+
+            q += query
+            p.extend( constraints )
+
+        try:
+            c = self.db.cursor()
+            c.execute( q, p )
+        except sqlite3.OperationalError, ex:
+            raise QueryError( ex )
+
+    def select( self, rows = None, query = None, order = None, group = None, descending = False, distinct = False ):
+
+        s = Selection( rows, query, order, group, descending, distinct )
+        return Query( s, self )
+
+# vim:sts=4:et:sw=4
---
+++ lib/higu.py
@@ -0,0 +1,296 @@
+import filemgmt
+import os
+
+from hash import calculate_details
+from filemgmt import ResultIterator
+
+DEFAULT_ENVIRON = os.path.join( os.environ['HOME'], '.higu' )
+HIGURASHI_DB_NAME = 'hfdb.dat'
+HIGURASHI_DATA_PATH = 'imgdat'
+
+class File:
+
+    def __init__( self, db, id ):
+
+        self.db = db
+        self.id = id
+
+    def get_id( self ):
+
+        return self.id
+
+    def get_parent( self ):
+
+        parent = self.db.db.get_mfl().get_parent( self.id )
+
+        if( parent == None ):
+            return None
+        else:
+            return File( self.db, parent )
+
+    def set_parent( self, parent ):
+
+        if( isinstance( parent, File ) ):
+            parent = parent.id
+
+        self.db.db.get_mfl().set_parent( self.id, parent )
+
+    def set_duplicate_of( self, parent ):
+
+        if( isinstance( parent, File ) ):
+            parent = parent.id
+
+        self.db.db.get_mfl().set_parent( self.id, parent, filemgmt.ORDER_DUPLICATE )
+
+    def set_varient_of( self, parent ):
+
+        if( isinstance( parent, File ) ):
+            parent = parent.id
+
+        self.db.db.get_mfl().set_parent( self.id, parent, filemgmt.ORDER_VARIENT )
+
+    def is_duplicate( self ):
+
+        return self.db.db.get_mfl().get_order( self.id ) == filemgmt.ORDER_DUPLICATE
+
+    def is_varient( self ):
+
+        return self.db.db.get_mfl().get_order( self.id ) == filemgmt.ORDER_VARIENT
+
+    def child_iterator( self ):
+
+        return ResultIterator( self.db.db.get_mfl().child_iterator( self.id ),
+                lambda x: File( self.db, x ) )
+
+    def get_name( self ):
+
+        try:
+            iter = self.get_names()
+            name = iter.next()
+            try:
+                while 1:
+                    iter.next()
+            except StopIteration:
+                return name
+        except StopIteration:
+            raise 'blah'
+            p = self.get_path()
+            if( p == None ):
+                name = 'unknown'
+            else:
+                name = os.path.split( p )[-1]
+
+    def get_names( self ):
+
+        return self.db.db.get_naml().lookup_names( self.id )
+
+    def get_length( self ):
+
+        return self.db.db.get_mfl().details( self.id )[0]
+
+    def get_hash( self ):
+
+        return self.db.db.get_mfl().details( self.id )[3]
+
+    def get_tags( self ):
+
+        return self.db.db.get_tagl().lookup_tags( self.id )
+
+    def tag( self, tag ):
+
+        return self.db.db.get_tagl().tag( self.id, tag )
+
+    def untag( self, tag ):
+
+        return self.db.db.get_tagl().untag( self.id, tag )
+
+    def get_path( self ):
+
+        try:
+            d = self.db._get_path_for_id( self.id )
+            ls = os.listdir( d )
+            ids = '%016x' % ( self.id )
+        except OSError:
+            return None
+
+        for f in ls:
+            try:
+                if( f.index( ids ) == 0 ):
+                    return os.path.join( d, f )
+            except ValueError:
+                pass
+
+        return None
+
+    def __eq__( self, o ):
+
+        return self.db == o.db and self.id == o.id
+
+class Database:
+
+    def __init__( self, environ ):
+
+        self.environ = environ
+
+        if( not os.path.isdir( self.environ ) ):
+            os.makedirs( self.environ )
+
+        self.db = filemgmt.FileMgmtDb( os.path.join( environ, HIGURASHI_DB_NAME ) )
+
+    def _get_path_for_id( self, id ):
+
+        lv2 = (id >> 12) % 0xfff
+        lv3 = (id >> 24) % 0xfff
+        lv4 = id >> 36
+
+        assert lv4 == 0
+
+        return os.path.join( self.environ, HIGURASHI_DATA_PATH, '%03x' % ( lv3 ), \
+                                                                '%03x' % ( lv2 ) )
+
+    def _load_data( self, path, id ):
+
+        tgt_path = self._get_path_for_id( id )
+        if( not os.path.isdir( tgt_path ) ):
+            os.makedirs( tgt_path )
+
+        name = os.path.split( path )[-1]
+        try:
+            ext = name[name.rindex( '.' ):]
+        except ValueError:
+            ext = '.dat'
+
+        tgt = os.path.join( tgt_path, '%016x%s' % ( id, ext ) )
+
+        os.rename( path, tgt )
+
+    def commit( self ):
+
+        self.db.commit()
+
+    def close( self ):
+
+        self.db.close()
+
+    def get_file_by_id( self, id ):
+
+        return File( self, id )
+
+    def lookup_files_by_details( self, len = None, crc32 = None, md5 = None, sha1 = None ):
+
+        mfl = self.db.get_mfl()
+        return ResultIterator( mfl.lookup( len, crc32, md5, sha1 ),
+                lambda x: File( self, x ) )
+
+    def lookup_files_by_name( self, name ):
+
+        return [].__iter__()
+
+    def lookup_files_by_tags( self, require, add = [], sub = [], strict = False ):
+
+        q = self.db.get_tagl().restrict_ids( self.db.get_mfl().mfl, require, add, sub, strict )
+        return ResultIterator( q.__iter__(), lambda x: File( self, x[0] ) )
+
+    def lookup_files_by_tags_with_names( self, require, add = [], sub = [], strict = False ):
+
+        q = self.db.get_tagl().restrict_ids( self.db.get_mfl().mfl, require, add, sub, strict )
+        q = self.db.get_naml().lookup_names_by_query( q )
+
+        class ResultWithNameIterator:
+
+            def __init__( iself, iter ):
+
+                iself.iter = iter
+
+            def __iter__( iself ):
+
+                return iself
+
+            def next( iself ):
+
+                id, name = iself.iter.next()
+                f = File( self, id )
+                if( name == None ):
+                    name = f.get_name()
+                return f, name
+
+        return ResultWithNameIterator( q.__iter__() )
+
+    def lookup_untagged_files( self ):
+
+        mfl = self.db.get_mfl()
+        tagl = self.db.get_tagl()
+        
+        all = mfl.lookup()
+        tagged = [i for i in tagl.lookup_ids( [] )]
+
+        class UntaggedIterator:
+
+            def __init__( self, all_iter, tagged ):
+
+                self.all_iter = all_iter
+                self.tagged = tagged
+
+            def next( self ):
+
+                while( 1 ):
+                    n = self.all_iter.next()
+                    if( n not in self.tagged ):
+                        return n
+
+        untagged = [f for f in all if f not in tagged]
+
+        return ResultIterator( UntaggedIterator( all, tagged ),
+                lambda x: File( self, x ) )
+
+    def all_files( self ):
+
+        mfl = self.db.get_mfl()
+        return ResultIterator( mfl.lookup(), lambda x: File( self, x ) )
+
+    def all_tags( self ):
+
+        tagl = self.db.get_tagl()
+        return tagl.all_tags()
+
+    def register_file( self, path ):
+
+        name = os.path.split( path )[1]
+
+        details = calculate_details( path )
+        results = self.lookup_files_by_details( *details )
+
+        mfl = self.db.get_mfl()
+        naml = self.db.get_naml()
+
+        try:
+            id = results.next().get_id()
+        except StopIteration:
+            id = mfl.register( *details )
+
+        naml.register( id, name )
+        self.commit()
+
+        f = File( self, id )
+        if( f.get_path() == None ):
+            self._load_data( path, id )
+
+        return f
+
+    def rename_tag( self, tag, new_name ):
+
+        tagl = self.db.get_tagl()
+        return tagl.rename_tag( tag, new_name )
+
+def init_default():
+
+    return Database( DEFAULT_ENVIRON )
+
+def compare_details( a, b ):
+
+    return long( a[0] ) == long( b[0] ) \
+       and str( a[1] ) == str( b[1] ) \
+       and str( a[2] ) == str( b[2] ) \
+       and str( a[3] ) == str( b[3] )
+
+# vim:sts=4:sw=4:et
