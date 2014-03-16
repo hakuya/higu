b9c9c69f1dc3a9544b8760f3e77a7ff01fddcdea
Erik Miranda <1644114+hakuya@users.noreply.github.com>
2014-03-16 15:21:05 -0400

    Fix resizing image when window is resized

--- static/image.js
+++ static/image.js
@@ -1,67 +1,112 @@
-function set_image_zoom( tab, zoom )
+/**
+ * class Viewer
+ */
+ImageViewer = function( tab, im )
 {
-    im = tab.data( 'im' );
+    /**
+     * apply_zoom( zoom ) - zoom the image to the given amount
+     */
+    this.apply_zoom = function( zoom )
+    {
+        this.im.width = this.im_width * zoom;
+        this.im.height = this.im_height * zoom;
+    };
 
-    if( !im ) return;
+    this.compute_zoom = function()
+    {
+        if( this.zoom == 'fit_inside' || this.zoom == 'fit_outside' ) {
+            tab_div = this.tab.find( '.tab' );
+            info_div = this.tab.find( '.info' );
 
-    tab.data( 'im-zoom', zoom );
+            container_width = this.tab.width() - info_div.width() - 20;
+            container_height = this.tab.height();
 
-    im.width = tab.data( 'im-width' ) * zoom;
-    im.height = tab.data( 'im-height' ) * zoom;
-}
+            width_ratio = 1.0 * container_width / this.im_width;
+            height_ratio = 1.0 * container_height / this.im_height;
 
-function resize_image( tab, zoom )
-{
-    im = tab.data( 'im' );
-    if( !im ) return;
-
-    switch( zoom ) {
-        case 0:
-            set_image_zoom( tab, 1.0 );
-            break;
-        case -1:
-        case -2:
-            tab_div = tab.find( '.tab' );
-            info_div = tab.find( '.info' );
-
-            container_width = tab.width() - info_div.width();
-            container_height = tab.height();
-
-            img_width = tab.data( 'im-width' );
-            img_height = tab.data( 'im-height' );
-
-            width_ratio = 1.0 * container_width / img_width;
-            height_ratio = 1.0 * container_height / img_height;
-
-            if( zoom == -1 ) {
+            if( this.zoom == 'fit_inside' ) {
                 if( width_ratio < height_ratio ) {
-                    set_image_zoom( tab, width_ratio );
+                    return width_ratio;
                 } else {
-                    set_image_zoom( tab, height_ratio );
+                    return height_ratio;
                 }
             } else {
                 if( width_ratio < height_ratio ) {
-                    set_image_zoom( tab, height_ratio );
+                    return height_ratio;
                 } else {
-                    set_image_zoom( tab, width_ratio );
+                    return width_ratio;
                 }
             }
-            break;
-        default:
-            set_image_zoom( tab, tab.data( 'im-zoom' ) * zoom );
-            break;
+        } else {
+            return this.zoom;
+        }
     }
-}
+
+    /**
+     * refresh() - reapply the set zoom mode
+     */
+    this.refresh = function()
+    {
+        this.apply_zoom( this.compute_zoom() );
+    };
+
+    /**
+     * set_zoom( zoom ) - set the zoom mode. If zoom is a positive number, the
+     *   image is set to that zoom level. If zoom is a negative number, the
+     *   image is zoomed by the given amount relative to the current zoom.
+     *   Otherwise, zoom may be 'fit_inside' or 'fit_outside' which zoom the
+     *   image relative to the container.
+     */
+    this.set_zoom = function( zoom )
+    {
+        if( zoom == 'fit_inside' || zoom == 'fit_outside' || zoom > 0 ) {
+            this.zoom = zoom;
+        } else if( zoom < 0 ) {
+            this.zoom = this.compute_zoom();
+            this.zoom *= -zoom;
+        } else {
+            // do nothing
+        }
+
+        this.refresh();
+    };
+
+    this.im = im;
+    this.tab = tab;
+
+    this.im_width = this.im.width;
+    this.im_height = this.im.height;
+    this.zoom = 'fit_inside';
+
+    tab.data( 'viewer', this );
+
+    this.refresh();
+};
 
 function register_image( im )
 {
-    elem = $( im );
-    tab = elem.closest( '.tab' );
+    tab = $( im ).closest( '.tab' );
+    return new ImageViewer( tab, im );
+}
 
-    tab.data( 'im', im );
-    tab.data( 'im-width', im.width );
-    tab.data( 'im-height', im.height );
-    tab.data( 'im-zoom', 1.0 );
+function get_viewer( elem )
+{
+    tab = $( elem ).closest( '.tab' );
+    return tab.data( 'viewer' );
+}
 
-    resize_image( tab, -1 );
+function resize_image( elem, zoom )
+{
+    viewer = get_viewer( elem );
+    if( viewer ) {
+        viewer.set_zoom( zoom );
+    }
+}
+
+function refresh_image( elem )
+{
+    viewer = get_viewer( elem );
+    if( viewer ) {
+        viewer.refresh();
+    }
 }
